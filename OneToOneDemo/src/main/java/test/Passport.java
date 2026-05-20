420775912ec853857addf6ed9ce7419c5d70a6fa
BangarYB17 <bangaryogeshwar3@gmail.com>
2026-05-20 09:20:46 +0530

    Relational Mapping: 2) one to one

---
+++ OneToOneDemo/src/main/java/test/Passport.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Passport {
+	private int pass_id;
+	private String p_number;
+
+	public int getPass_id() {
+		return pass_id;
+	}
+
+	public void setPass_id(int pass_id) {
+		this.pass_id = pass_id;
+	}
+
+	public String getP_number() {
+		return p_number;
+	}
+
+	public void setP_number(String p_number) {
+		this.p_number = p_number;
+	}
+
+}
---
+++ OneToOneDemo/src/main/java/test/StoreData.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class StoreData {
+
+	public static void main(String[] args) {
+		Configuration conf= new Configuration();
+		conf.configure();
+		
+		SessionFactory factory = conf.buildSessionFactory();
+		
+		Session session = factory.openSession();
+		
+		Transaction tx = session.beginTransaction();
+		
+		Person p1 = new Person();
+		p1.setName("Lekhit");
+		
+		Passport ps1 = new Passport();
+		ps1.setP_number("AB123");
+
+		p1.setPassport(ps1);
+
+		session.save(p1);
+		tx.commit();
+		
+		session.close();
+		factory.close();
+		
+		System.out.println("Data Inserted Successfully!");
+	}
+
+}
